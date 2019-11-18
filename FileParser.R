863f06f57872f3e4672e9514194e5db58d38703a
benjaminhansen <benjamminhansen@gmail.com>
2019-11-18 15:55:09 -0700

    Loop through sub files of folders

--- FileParser.R
+++ FileParser.R
@@ -1,1 +1,19 @@
 #ToDo File Parsing Benjamin
+library(readtext)
+
+# Load the dataset to append Columns to
+complete_speech_dataset <- read.csv(file = "")
+
+#Use "r" for reading in text mode
+DATA_DIR <- system.file("extdata/", package = "readtext")
+
+allFolders <- list.dirs(path = "raw data")
+
+# For folder in folders and file in folder
+for (folder in allFolders) {
+  for (file in list.files(path = folder, pattern = "*.wav")) {
+    # Do stuff to that file character vector!
+    # Open as you please...
+  }
+}
+
