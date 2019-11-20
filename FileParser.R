8ca203213dcd63ca90f68a48609572d307ce2df5
Jon STuTZ <stu15012@byui.edu>
2019-11-20 11:15:34 -0800

    almost done Jon just trying to help

--- FileParser.R
+++ FileParser.R
@@ -1,19 +1,38 @@
 #ToDo File Parsing Benjamin
-library(readtext)
+#library(readtext)
+library(optimbase)
+#library(tidyverse)
+library(stringr)
 
 # Load the dataset to append Columns to
-complete_speech_dataset <- read.csv(file = "")
+#complete_speech_dataset <- read.csv(file = "")
 
 #Use "r" for reading in text mode
-DATA_DIR <- system.file("extdata/", package = "readtext")
+#DATA_DIR <- system.file("extdata/", package = "readtext")
 
 allFolders <- list.dirs(path = "raw data")
 
+# initialize values matches the length when adding rows
+dataFrame <- data.frame(2, 3, 4, 5, 6)
+# works better if column names match when adding rows
+colnames(dataFrame) <- c("X3", "X4", "X5", "X6", "X7")
 # For folder in folders and file in folder
-for (folder in allFolders) {
+for (folder in allFolders[-1]) {
+  print(basename(folder))
   for (file in list.files(path = folder, pattern = "*.wav")) {
     # Do stuff to that file character vector!
     # Open as you please...
+    data <- as.character(basename(file))
+    # splits on every - or .
+    dataVector <- (strsplit(data, regex("\\-|\\."))) 
+    # drops the first two columns and last column and adds the row to the data frame
+    dataFrame <- rbind(dataFrame, data.frame(transpose(matrix(unlist(dataVector))))[-1][-1][-6]) 
+    break
   }
+  break
 }
-
+# put meaningfull names on the columns
+colnames(dataFrame) <- c("Emotion", "intensity", "statement", "repetition" , "actor")
+# drops the first row of garbage values
+dataFrame <- dataFrame[-1,]
+View(dataFrame)
