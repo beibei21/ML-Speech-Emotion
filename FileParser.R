3596471663607f4352ef73d2afc65ad4e044f174
benjaminhansen <benjamminhansen@gmail.com>
2019-11-22 15:58:47 -0700

    Merge two data frames

--- FileParser.R
+++ FileParser.R
@@ -7,27 +7,45 @@ library(stringr)
 allFolders <- list.dirs(path = "raw data")
 
 # initialize values matches the length when adding rows
-dataFrame <- data.frame(2, 3, 4, 5, 6) # Our Template
+fileData <- data.frame(2, 3, 4, 5, 6) # Our Template, for file name data
+waveData <- data.frame(2,3,3,4,5) # Our Template, for wave data
+
 
 # works better if column names match when adding rows
-colnames(dataFrame) <- c("X3", "X4", "X5", "X6", "X7")
+colnames(fileData) <- c("X3", "X4", "X5", "X6", "X7") # more templates
 
 # For folder in folders and file in folder
 # Loop through all folders except the first one (which is "raw data")
 # Subtract it out
+# Create two data frames: 1 for the wave data, 1 for the file name data
 for (folder in allFolders[-1]) {
   print(basename(folder))
   for (file in list.files(path = folder, pattern = "*.wav")) {
-    # Do stuff to that file character vector!
+    ### Process and Add data to file name dataframe
     data <- as.character(basename(file))
     # splits on every - or .
     dataVector <- (strsplit(data, regex("\\-|\\.")))
     # drops the first two columns and last column and adds the row to the data frame
-    dataFrame <- rbind(dataFrame, data.frame(transpose(matrix(unlist(dataVector))))[-1][-1][-6])
-  }
+    # chop off what we don't want
+    # row bind to the dataFrame
+    fileData <- rbind(fileData, data.frame(transpose(matrix(unlist(dataVector))))[-1][-1][-6])
+    
+    ### Process and Add data to wave dataframe
+    }
 }
 # put meaningfull names on the columns
-colnames(dataFrame) <- c("Emotion", "intensity", "statement", "repetition" , "actor")
+colnames(fileData) <- c("Emotion", "intensity", "statement", "repetition" , "actor")
 # drops the first row of garbage values (our template integers)
-dataFrame <- dataFrame[-1,]
-View(dataFrame)
+fileData <- fileData[-1,]
+
+# Delete this line
+
+
+# replae second frame with waveData
+fileData <- data.frame(c(fileData, waveData))
+
+View(fileData)
+
+
+
+
