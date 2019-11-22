3b1ee0c9ed4faa56fb5bdf42641c7b1527708d85
benjaminhansen <benjamminhansen@gmail.com>
2019-11-22 14:54:18 -0700

    Deleted comments...

--- FileParser.R
+++ FileParser.R
@@ -11,17 +11,19 @@ dataFrame <- data.frame(2, 3, 4, 5, 6) # Our Template
 
 # works better if column names match when adding rows
 colnames(dataFrame) <- c("X3", "X4", "X5", "X6", "X7")
+
 # For folder in folders and file in folder
+# Loop through all folders except the first one (which is "raw data")
+# Subtract it out
 for (folder in allFolders[-1]) {
   print(basename(folder))
   for (file in list.files(path = folder, pattern = "*.wav")) {
     # Do stuff to that file character vector!
-    # Open as you please...
     data <- as.character(basename(file))
     # splits on every - or .
-    dataVector <- (strsplit(data, regex("\\-|\\."))) 
+    dataVector <- (strsplit(data, regex("\\-|\\.")))
     # drops the first two columns and last column and adds the row to the data frame
-    dataFrame <- rbind(dataFrame, data.frame(transpose(matrix(unlist(dataVector))))[-1][-1][-6]) 
+    dataFrame <- rbind(dataFrame, data.frame(transpose(matrix(unlist(dataVector))))[-1][-1][-6])
   }
 }
 # put meaningfull names on the columns
