64fee71e5b8d3d8b0e06e66b8171c816345c0610
benjaminhansen <benjamminhansen@gmail.com>
2019-12-09 14:27:44 -0700

    Added lambda functions

--- audioDataSetSaver.R
+++ audioDataSetSaver.R
@@ -8,8 +8,14 @@ source("FileParser.R")
 
 # Prepare the data in this one line
 # 1 person for now
-prepareData <- function(person = 24) {
-  myData <- getWaveData(person) # ta da
+prepareData <- function(person = 24, call1 = TRUE, slice = 10) {
+  ## Define a lambda to pass to the getWave function
+  if (!call1) {
+    waveFunToCall = waveToNumWithSlicesAdded
+  } else {
+    waveFunToCall = waveToNum
+  }
+  myData <- getWaveData(person, waveFunToCall, slice = slice) # ta da
   # Now save the whole data frame to a file: emotion.csv
   # col.names = TRUE is set by default. We want the colnames but not the row names
   # The row names will just be 1,2,3,4,... useless.
