83560d22ee0bb0d3581571319e5f6ef1bf036905
Shania <mau14002@byui.edu>
2019-12-11 16:10:22 -0700

    new

--- svm.R
+++ svm.R
@@ -70,50 +70,6 @@ trainData <- myData[-testRows,]
 }
 
 
-
-
-# # the following are to encode the emotions to 2 emotions
-# # Emotion 1 will be happy comprised of neutral, calm, happy, surprised
-# myData[myData == 2] <- 1
-# myData[myData == 3] <- 1
-# myData[myData == 8] <- 1
-# 
-# #Emotion 2 will be sad comrised of sad, angry, fearful, disgust
-# myData[myData == 4] <- 2
-# myData[myData == 5] <- 2
-# myData[myData == 6] <- 2
-# myData[myData == 7] <- 2
-
-# the following are to encode the emotions to 4 emotions
-# Emotion 1 will be neutral comprised of neutral, calm
-#myData[myData == 2] <- 1
-# Emotion 3 will be happy comprised of happy, sad
-#myData[myData == 4] <- 3
-# Emotion 5 will be angry comprised of angry, fearful
-#myData[myData == 6] <- 5
-# Emotion 7 will be angry comprised of disgust, surprised
-#myData[myData == 8] <- 7
-
-#### we want to go through all combinations on pair two emotions together and print out the best accurracy pair ###
-
-
-
-
-
-
-# Scale the DAta 
-#already scaled It does not do any better to scale again
-# trainData[-1] = scale(trainData[-1])
-# testData[-1] = scale(testData[-1])
-# print(formula(trainData))
-
-
-
-### use hyper parameters
-
-
-
-
 # obj <- tune(svm, emotion~., data = trainData, 
 #             ranges = list(gamma = 2^(-1:1), cost = 2^(2:4), nu = seq(0.01,0.5, .1),
 #             tunecontrol = tune.control(sampling = "fix")))
@@ -122,11 +78,3 @@ trainData <- myData[-testRows,]
 
 # summary(obj)
 # plot(obj)
-
-
-
-# 
-#table(predict(classifier), myData$Emotion, dnn=c("Prediction", "Actual"))  
-
-# this didn't work 
-#confusionMatrix(myData$Emotion, predict(classifier))
