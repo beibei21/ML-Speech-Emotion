6bd5f6166a7ec723369b4eb18fe62136b7c8744f
benjaminhansen <benjamminhansen@gmail.com>
2019-12-11 21:08:42 -0700

    Tried linear.output = TRUE Used compute() instead of predict()

--- neural_network.R
+++ neural_network.R
@@ -10,10 +10,11 @@ source("audioDataSetSaver.R") # Run this file
 
 # data needs to be normalized first and targets label encoded
 myData = getData() ### this line is for debugging perposes only ###
-
+myData <- numericallyEncode(myData)
 # get test data and train data
 numRows = 1:nrow(myData) 
-testRows <- sample(numRows, trunc(length(numRows)* 0.3))
+print(paste("Number of rows: ", nrow(myData)))
+testRows <- sample(numRows, trunc(length(numRows) * 0.3))
 testData <- myData[testRows,]
 trainData <- myData[-testRows,]
 
@@ -21,6 +22,8 @@ trainData <- myData[-testRows,]
 # network is a list
 # look up nerualnet:neuralnet lots of options to change
 # - formula(trainData) gives a blue print of what the data looks like, returns type formula
+#    - more specifically it will denote for the nueralnet() that the emotion (our first column)
+#    - is the target column and the rest of the columns are the training columns
 # - col1 + col2 = the futures  ## I think this is mainly used for the plotting the network model
 # - hidden = # of nodes in the layer
 # - act.fct = "logistic" used for smoothing the result
@@ -28,20 +31,30 @@ trainData <- myData[-testRows,]
 # - Classification, not regression
 # - learningrate = value of learning rate used on;y for back propagation
 network <- neuralnet(formula(trainData), data = trainData, learningrate = .1, 
-                     hidden = c(15,10,8), linear.output = FALSE)
+                     hidden = c(15,10,8), linear.output = TRUE)
 
 # see what the network looks like
 plot(network)
 
 
 #get predictions
-predictions = predict(network, testData[,-1]) # remove the emotion
-#table(testData$Emotion, apply(predictions, 1, which.max))
+network.results <- compute(network, testData[,-1]) # remove the emotion
 
+# Or try this
+results <- data.frame(actual = testData$emotion, prediction = nn.results$net.result) 
 
-confusionMatrix <- table(pred = predictions, true = testData$Emotion)
+# View rounded results
+roundedResults <- sapply(results, round, digits = 0)
+roundedResultsdf <- data.frame(roundedResults)
+attach(roundedResultsdf)
+table(actual, prediction) # what is prediction
+  
+# See the net result
+print(accuracy <- predictions$net.result)
+#table(testData$, apply(predictions, 1, which.max))
+confusionMatrix <- table(pred = predictions, true = testData$emotion)
 
-agreement <- predictions == testData$Emotion
+agreement <- predictions == testData$emotion
 accuracy <- prop.table(table(agreement))
 
 print(confusionMatrix)
