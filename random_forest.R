3f06780c0e3ec86e8ecb97160b7a9bf3d8ac6cab
Shania <mau14002@byui.edu>
2019-12-14 17:25:39 -0700

    new additions

--- random_forest.R
+++ random_forest.R
@@ -53,16 +53,16 @@ confusionMatrix(testData$pred, as.factor(testData$emotion))
 library(readr)
 emotion_extended <- read_csv("emotion_extended.csv") 
 
-emotion_extended$gender[emotion_extended$gender=="male"] <- 1
-emotion_extended$gender[emotion_extended$gender=="female"] <- 0
+emotion_extended$gender[emotion_extended$gender == "male"] <- 1
+emotion_extended$gender[emotion_extended$gender == "female"] <- 0
 
 emotion_extended$gender <- as.numeric(emotion_extended$gender)
 
 # Get test data and train data
 numRows2 = 1:nrow(emotion_extended) 
-testRows2 <- sample(numRows, trunc(length(numRows) * 0.3))
-testData2 <- emotion_extended[testRows,]
-trainData2 <- emotion_extended[-testRows,]
+testRows2 <- sample(numRows2, trunc(length(numRows2) * 0.3))
+testData2 <- emotion_extended[testRows2,]
+trainData2 <- emotion_extended[-testRows2,]
 
 # Scale the DAta 
 #trainData[-1] <- scale(trainData2[-1])
@@ -71,7 +71,7 @@ trainData2 <- emotion_extended[-testRows,]
 
 # random forest 
 # ntree = 1000
-forests2 <- randomForest(formula = as.factor(emotion) ~ ., ntree = 50, data = trainData2)
+forests2 <- randomForest(formula = as.factor(emotion) ~ ., ntree = 15, data = trainData2)
 # Calm = 2 has the least amount of error at 65% error 
 # Sad =  4 has 83% error 
 # Happy = 3 82% error
@@ -86,11 +86,35 @@ forests2 <- randomForest(formula = as.factor(emotion) ~ ., ntree = 50, data = tr
 #Fearful greatest amount of error 90%
 # Angry second least amount of error 71%
 
+library(caret) # for confusionMatrix
 # trying to do a confusion matrix and show accuracy as well 
-testData$pred <- predict(forests2, testData2)
-testData$pred <- as.factor(testData2$pred)
+testData2$pred <- predict(forests20, testData2)
+testData2$pred <- as.factor(testData2$pred)
 confusionMatrix(testData2$pred, as.factor(testData2$emotion))
-# 22.45% accuracy 
+# 87.87 % with 10 trees 
+# 95.31 % with 20 trees
+# 
+
+forests3 <- randomForest(formula = as.factor(emotion) ~ ., ntree = 10, data = trainData2)
+
+forests20 <- randomForest(formula = as.factor(emotion) ~ ., ntree = 20, data = trainData2)
+# COOB 16.63%
+
+forests30 <- randomForest(formula = as.factor(emotion) ~ ., ntree = 30, data = trainData2)
+
+testData2$pred <- predict(forests30, testData2)
+testData2$pred <- as.factor(testData2$pred)
+confusionMatrix(testData2$pred, as.factor(testData2$emotion))
+
+forests40 <- randomForest(formula = as.factor(emotion) ~ ., ntree = 40, data = trainData2)
+
+testData2$pred <- predict(forests40, testData2)
+testData2$pred <- as.factor(testData2$pred)
+confusionMatrix(testData2$pred, as.factor(testData2$emotion))
 
 
+forests50 <- randomForest(formula = as.factor(emotion) ~ ., ntree = 50, data = trainData2)
 
+testData2$pred <- predict(forests50, testData2)
+testData2$pred <- as.factor(testData2$pred)
+confusionMatrix(testData2$pred, as.factor(testData2$emotion))
